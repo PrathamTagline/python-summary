236600521e2bb506c30f513e54d9181b4455976a
PATEL PRATHAM <patelpratham429@gmail.com>
2025-01-23 21:55:02 +0530

    count vowel without loop

---
+++ count-vowel.py
@@ -0,0 +1,23 @@
+string = "hello my name is pratham, i am a student of btech.my hobby is to read books and play genshin impact , hahahahahaha"
+vowel_dicttionary = {}
+
+string.replace(" ", "")
+new_string = string.lower()
+count_a = new_string.count("a")
+count_e = new_string.count("e")
+count_i = new_string.count("i")
+count_o = new_string.count("o")
+count_u = new_string.count("u")
+
+vowel_dicttionary["a"] = count_a
+vowel_dicttionary["e"] = count_e
+vowel_dicttionary["i"] = count_i
+vowel_dicttionary["o"] = count_o
+vowel_dicttionary["u"] = count_u
+
+print(vowel_dicttionary)
+
+
+
+
+
