0c037f12fc34172079b3cd48e1fc04bf3a076fbf
PATEL PRATHAM <patelpratham429@gmail.com>
2025-01-23 22:56:33 +0530

    simple commit

--- temp.js
+++ temp.js
@@ -1,4 +1,16 @@
-String = "aaabbbiii";
+String = "aaabbbiiidsnowfnowowerworuworjwohicnscmsojpscpkxsnowfwpfwonfbowbfownfwpfmwfnqeqeuwryuiewurowndwncbviagsdodjwoeirowqewnbcvbowvwpdwufiwoufpwj";
+charcter_arr = String.split("");
+map = {}
+total_a = charcter_arr.filter(x => x === "a").length;
+total_e = charcter_arr.filter(x => x === "e").length;
+total_i = charcter_arr.filter(x => x === "i").length;
+total_o = charcter_arr.filter(x => x === "o").length;
+total_u = charcter_arr.filter(x => x === "u").length;
 
-a = String.split('').reduce((acc, curr) => acc + curr.count("a"), 0);
-console.log(a);
+map.a = total_a;
+map.e = total_e;
+map.i = total_i;
+map.o = total_o;
+map.u = total_u;
+
+console.log(map);
